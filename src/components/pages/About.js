52be147002f908e5e1f914744024e51466c28a48
Krystopher Quintero <krystopher.q@gmail.com>
2023-03-01 17:00:27 -0800

    ignore conatce form

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,6 +2,18 @@ import React from "react";
 
 function Me(){
   return(
-    
+    <section id="bio">
+            <div className="w-full bg-base-200">
+                <div className="card-body w-5/6 m-auto">
+                    <h2 className="text-3xl">About me: </h2>
+                    <p>
+                        My Name is Krystopher Quintero. I am a beginner/intermediate programmer. I am seeking for new job oppurtunities to further my knowledge and expirence in programming. I am open to working remotely, hybrid, or in-office wherever the oppurtunities are greatest. I work great in teams and am a quick learner. Please feel free to browse my website I created and feel free to leave an email for any employment oppurtunities you might have to offer. Thank You! 
+                    </p>
+                </div>
+            </div>
+        </section>
   )
 }
+
+
+export default Me;
