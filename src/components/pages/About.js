2e65adbd365f83b9605ed1b0d2c45edbcc8d06e6
Krystopher Quintero <krystopher.q@gmail.com>
2023-03-03 17:42:57 -0800

    fix spelling err

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,7 @@ function Me(){
                 <div className="card-body w-5/6 m-auto">
                     <h2 className="text-3xl">About me: </h2>
                     <p>
-                        My Name is Krystopher Quintero. I am a beginner/intermediate programmer. I am seeking for new job oppurtunities to further my knowledge and expirence in programming. I am open to working remotely, hybrid, or in-office wherever the oppurtunities are greatest. I work great in teams and am a quick learner. Please feel free to browse my website I created and feel free to leave an email for any employment oppurtunities you might have to offer. Thank You! 
+                    My Name is Krystopher Quintero. I am a beginner/intermediate programmer. Currently, I am seeking new job opportunities to further my knowledge and expand my experience in programming. I am open to working remotely, hybrid, or in-office. I work great in teams and I am a quick learner able to adapt to project needs. Please feel free to browse my website and leave an email for any available employment opportunities. Thank You! 
                     </p>
                 </div>
             </div>
