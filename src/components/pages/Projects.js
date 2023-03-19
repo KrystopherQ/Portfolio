aa9bf4ffcbe98d046c93363b53e11457c743e9d0
Krystopher Quintero <krystopher.q@gmail.com>
2023-03-19 16:26:40 -0700

    fix link

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -14,11 +14,11 @@ function Projects() {
                 <div className="card-body w-5/6 m-auto">
                     <h2 className="text-3xl">Projects: </h2>
                     <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-around">
-                        <Project imgPreview={ecommerce} name="E-commerce Back-end" repoLink="https://github.com/KrystopherQ/crispy-doodle" />
+                        <Project imgPreview={ecommerce} name="E-commerce Back-end" repoLink="https://github.com/KrystopherQ/crispy-doodle" deployedLink="https://github.com/KrystopherQ/crispy-doodle"/>
                         <Project imgPreview={pethub} name="Pethub" repoLink="https://github.com/KrystopherQ/pethub" deployedLink="https://krystopherq.github.io/pethub/" />
                         <Project imgPreview={pass} name="Password Generator" repoLink="https://github.com/KrystopherQ/Greatest-Generator" deployedLink="https://krystopherq.github.io/Greatest-Generator"/>
                         <Project imgPreview={weather} name="Weather Dashboard" repoLink="https://github.com/KrystopherQ/KQSWA" deployedLink="https://krystopherq.github.io/KQSWA/" />
-                        <Project imgPreview={teamGen} name="Team Generator" repoLink="https://github.com/KrystopherQ/TPG-numbero1"/>
+                        <Project imgPreview={teamGen} name="Team Generator" repoLink="https://github.com/KrystopherQ/TPG-numbero1" deployedLink="https://github.com/KrystopherQ/TPG-numbero1"/>
                         <Project imgPreview={planner} name="Planner" repoLink="https://github.com/KrystopherQ/Work-Planner" deployedLink="https://krystopherq.github.io/Work-Planner/" />
                     </div>
                 </div>
